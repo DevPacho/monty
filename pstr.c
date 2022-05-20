3caf7b54a7d84c95ee3847bcfd5094c3a7815888
DevPacho <frankjose2004@gmail.com>
2022-05-19 23:04:40 -0500

    [FIXED] compilation

--- pstr.c
+++ pstr.c
@@ -9,10 +9,11 @@
 void pstr(stack_t **stack, unsigned int line_number)
 {
 	stack_t *traverse_to_print = *stack;
+	(void)line_number;
 
 	while (traverse_to_print && isprint(traverse_to_print->n) && traverse_to_print->n != 0)
 	{
-		printf("%s\n", traverse_to_print->n);
+		printf("%c\n", traverse_to_print->n);
 		traverse_to_print = traverse_to_print->next;
 	}
 }

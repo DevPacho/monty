d9b2ce68b0d1ee33282475dcb71a95508a558456
DevPacho <frankjose2004@gmail.com>
2022-05-19 20:44:00 -0500

    [ADDED] function prototype of pchar

--- monty.h
+++ monty.h
@@ -62,6 +62,7 @@ void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
 void match_operations(char *opcode, stack_t **stack, unsigned int line_number);
 
 #endif /*monty.h **/

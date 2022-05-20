5940105ef71d5e740b7436c2e0541a252c9bc6d4
DevPacho <frankjose2004@gmail.com>
2022-05-19 20:45:07 -0500

    function that prints chars

---
+++ pchar.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+* pchar - function that prints the char at the top of the stack.
+* @stack: head of the stack.
+* @line_number: line number for errors.
+*/
+
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack || !stack)
+	{
+		free_stack(*stack);
+		fclose(global.file);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if (!isalpha((*stack)->n))
+	{
+		free_stack(*stack);
+		fclose(global.file);
+		fprintf(stderr, "L%d: can't pchar, stack empty", line_number);
+	}
+
+	printf("%c", (*stack)->n);
+}
